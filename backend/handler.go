6f9982d7ea67265f635d0f667750bcd08eb86f55
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-29 17:23:20 -0400

    ls

--- backend/handler.go
+++ backend/handler.go
@@ -1,6 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"sync"
+)
+
+var mtx sync.Mutex
 
 func uploadPDFHandler(w http.ResponseWriter, r *http.Request) {
 
@@ -10,6 +17,34 @@ func downloadPDFHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+// List all PDFs availible
 func getPDFListHandler(w http.ResponseWriter, r *http.Request) {
+  if r.Method != http.MethodGet {
+    http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+    return
+  }
 
+  mtx.Lock()
+  files, err := os.ReadDir(uploadDir)
+  mtx.Unlock()
+
+  if err != nil {
+    http.Error(w, "Fail to read pdfs", http.StatusInternalServerError)
+    return
+  }
+
+  pdfs := make([]string, 0)
+
+  for _, file := range files {
+    if !file.IsDir() {
+      pdfs = append(pdfs, file.Name())
+    }
+  }
+
+  w.Header().Set("Content-Type", "application/json")
+  err = json.NewEncoder(w).Encode(pdfs)
+
+  if err != nil {
+    http.Error(w, "Fail to encode file names", http.StatusInternalServerError)
+  }
 }
