cce8558a24f7700ff8e91d9b5676ca4700cd94f2
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-29 10:32:22 -0400

    separation of concerns

--- cmd/main.go
+++ cmd/main.go
@@ -11,9 +11,8 @@ const PORT = 8080
 func main() {
   mux := http.NewServeMux()
 
-  mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("frontend/build"))))
-
   err := http.ListenAndServe(":" + strconv.FormatUint(PORT, 10), mux)
+
   if err != nil {
     log.Fatal(err)
   }
