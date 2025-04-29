71ff951836aa83bc6e7e1a721567e94bfaf09fde
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-29 16:55:01 -0400

    yer

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -10,10 +9,5 @@ const PORT = 8080
 
 func main() {
   mux := http.NewServeMux()
-
-  err := http.ListenAndServe(":" + strconv.FormatUint(PORT, 10), mux)
-
-  if err != nil {
-    log.Fatal(err)
-  }
+  http.ListenAndServe(":" + strconv.FormatUint(PORT, 10), mux)
 }
