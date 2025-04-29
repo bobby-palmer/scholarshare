7bf639352886c790a7eaec421905b43c768a53fa
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-04-29 17:06:34 -0400

    new struct

---
+++ backend/handler.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+func uploadPDFHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func downloadPDFHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func getPDFListHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+const uploadDir = "./uploads"
+const port = "8080"
+
+// Create uploads dir at boot if not already made
+func init() {
+  if err := os.MkdirAll(uploadDir, 0755); err != nil {
+    log.Fatal(err)
+  }
+}
 
 func main() {
-  fmt.Println("Hello")
+  http.ListenAndServe(":" + port, nil)
 }
