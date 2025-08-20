22213124f47967d055326d7916f11dee251c6927
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2025-08-20 13:13:22 -0400

    clean

--- backend/validation.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "mime/multipart"
-
-func validateFile(file multipart.File, header *multipart.FileHeader) bool {
-  return true
-}
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/bobby-palmer/scholarshare
-
-go 1.23.4
